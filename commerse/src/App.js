6f1ca677f256080d1d3823d802540faf61c45f02
Kawsar Hussen <kawsarhussen@gmail.com>
2019-08-16 15:52:20 -0400

    Header route added and some styling updated (margin and padding)

--- commerse/src/App.js
+++ commerse/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import Homepage from "./pages/homepage/homepage.js";
 import ShopPage from "./pages/page/shop.js";
-
+import Header from "./components/header/header.js";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 
@@ -10,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={ShopPage} />
