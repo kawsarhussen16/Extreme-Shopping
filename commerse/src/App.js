44bf6d323d2dde6ef4236a2a62058f140c634221
Kawsar Hussen <kawsarhussen@gmail.com>
2019-08-16 15:27:36 -0400

    Unncessary code deleted

--- commerse/src/App.js
+++ commerse/src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
-import "./App.css";
+
 import Homepage from "./pages/homepage/homepage.js";
+import ShopPage from "./pages/page/shop.js";
+
+import "./App.css";
 import { Route, Switch } from "react-router-dom";
 
-const HatsPage = () => (
-  <div>
-    <h1> Hello</h1>
-  </div>
-);
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Switch>
           <Route exact path="/" component={Homepage} />
-          <Route path="/hats" component={HatsPage} />
+          <Route path="/shop" component={ShopPage} />
         </Switch>
       </div>
     );
