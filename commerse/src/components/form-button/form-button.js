a276c1146ca5bbff1ba45a037be12390289e232f
Kawsar Hussen <kawsarhussen@gmail.com>
2019-08-20 14:18:42 -0400

    Goole sign in Button fully functional and style applied

--- commerse/src/components/form-button/form-button.js
+++ commerse/src/components/form-button/form-button.js
@@ -2,8 +2,11 @@ import React from "react";
 
 import "./form-button.style.scss";
 
-const FormButton = ({ children, ...allprops }) => (
-  <button className="custom-button" {...allprops}>
+const FormButton = ({ children, isGoogleSignIn, ...allprops }) => (
+  <button
+    className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+    {...allprops}
+  >
     {children}
   </button>
 );
