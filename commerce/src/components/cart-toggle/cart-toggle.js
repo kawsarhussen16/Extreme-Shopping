5e63bdbe76ef51defc29fbb09ad25663202bfd16
Kawsar Hussen <kawsarhussen@gmail.com>
2019-09-03 10:42:16 -0400

    Cart item imported in cart toggle comp, get access of cart from state to props

--- commerce/src/components/cart-toggle/cart-toggle.js
+++ commerce/src/components/cart-toggle/cart-toggle.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Button from '../form-button/form-button.js';
 import './cart-toggle.style.scss';
+import CartItem from '../cart-item/cart-item.js';
+import { connect } from 'react-redux';
 
-const CartToggle = () => (
+const CartToggle = ({ cartItems }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
+            {cartItems.map(item => (
+                <CartItem key={item.id} item={item} />
+            ))}
             <Button> CHECKOUT </Button>
         </div>
     </div>
 );
 
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+});
 
-export default CartToggle;
+export default connect(mapStateToProps)(CartToggle);
