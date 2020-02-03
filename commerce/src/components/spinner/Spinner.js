d87cc6bf99905bdfcb09c458b69e892174908f8d
Kawsar Hussen <kawsarhussen@gmail.com>
2020-02-03 01:10:05 -0500

    Spinner comp implemented

---
+++ commerce/src/components/spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { SpinnerContainer, SpinnerOverlay } from "./Spinner.style";
+
+const Spinner = () => (
+    <SpinnerOverlay>
+        <SpinnerContainer />
+    </SpinnerOverlay>
+);
+
+export default Spinner;
